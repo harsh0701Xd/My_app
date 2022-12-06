357cf458edb042045d7811fb80b35d466ee42efa
Harsh Raj <harshrajrac@gmail.com>
2022-12-06 17:06:07 +0530

    created database and admin login portal

--- My_app/core/models.py
+++ My_app/core/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your models here.
-class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+class Profile(models.Model): # the name of the database 
+    user = models.ForeignKey(User,on_delete=models.CASCADE) # the rows in the database
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
